f4911983d74059d9fe8ca427cc96b340d4e6066e
Danijela Boberic Krstićev <dboberic@uns.ac.rs>
2020-11-03 09:07:07 +0100

    rename servlet

--- TaskManagementServlet/src/dmi/ris/servlet/HelloCookie.java
+++ TaskManagementServlet/src/dmi/ris/servlet/HelloCookie.java
@@ -14,13 +14,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class HelloServlet
  */
 @WebServlet("/helloCookie")
-public class HelloServlet extends HttpServlet {
+public class HelloCookie extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloServlet() {
+    public HelloCookie() {
         super();
         // TODO Auto-generated constructor stub
     }
