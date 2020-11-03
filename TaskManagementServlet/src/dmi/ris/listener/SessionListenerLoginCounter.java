f9964944f3a52d4483985a1242f78bbbd2090ad2
Danijela Boberic Krstićev <dboberic@uns.ac.rs>
2020-11-03 12:02:17 +0100

    izmenjen session listener

--- TaskManagementServlet/src/dmi/ris/listener/SessionListenerLoginCounter.java
+++ TaskManagementServlet/src/dmi/ris/listener/SessionListenerLoginCounter.java
@@ -12,12 +12,17 @@ import dim.ris.model.User;
 @WebListener
 public class SessionListenerLoginCounter implements HttpSessionListener, HttpSessionAttributeListener{
 	ServletContext ctx=null;  
-    static int total=0,current=0; 
+    static int total=0;
+    static int current=1; 
     
 	@Override
 	public void attributeAdded(HttpSessionBindingEvent se) {
-		//User u=(User)se.getValue(); 
-		//System.out.println("logovao se korisnik " +u.getName()); 
+		User u=(User)se.getValue(); 
+		System.out.println("logovao se korisnik " +u.getName());
+		ctx=se.getSession().getServletContext();  
+		System.out.println("Do sada je ukupno puta pristupljeno aplikaciji "+ ctx.getAttribute("totalusers"));
+		System.out.println("Broj trenutno prijavljenih korisnika "+ ctx.getAttribute("currentusers"));
+
 	}
 
 	@Override
@@ -33,17 +38,17 @@ public class SessionListenerLoginCounter implements HttpSessionListener, HttpSes
 
 	@Override
 	public void sessionCreated(HttpSessionEvent se) {
-		/*total++;  
-	    current++; 
+		total = total +1;  
+	    current = current +1; 
 	    ctx=se.getSession().getServletContext();  
 	    ctx.setAttribute("totalusers", total);  
-	    ctx.setAttribute("currentusers", current);  */
+	    ctx.setAttribute("currentusers", current); 
 	}
 
 	@Override
 	public void sessionDestroyed(HttpSessionEvent se) {
 		 current--;  
-	     //ctx.setAttribute("currentusers",current);  
+	     ctx.setAttribute("currentusers",current);  
 	}
 
 }
